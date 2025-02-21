9b6ed9469d4948e4c38b8f9e1ef0f9659a4aa21c
bharath02233 <bharath02233@gmail.com>
2025-02-21 15:24:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function indexOfIgnoreCase(s1, s2) {
- return str.toLowerCase().indexOf(subStr.toLowerCase());
+ return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
